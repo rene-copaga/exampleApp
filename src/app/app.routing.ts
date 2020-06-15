da6103d9f0ef88e3474ebb01d3eea0214e0f73fb
Rene Copaga <rene.copaga@gmail.com>
2020-06-15 19:48:59 -0400

    Using Wildcards in Routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from "@angular/router";
 import { TableComponent } from "./core/table.component";
 import { FormComponent } from "./core/form.component";
+import { NotFoundComponent } from "./core/notFound.component";
 
 const routes: Routes = [
     { path: "form/:mode/:id", component: FormComponent },
     { path: "form/:mode", component: FormComponent },
-    { path: "", component: TableComponent }]
+    { path: "", component: TableComponent },
+    { path: "**", component: NotFoundComponent }]
 
 export const routing = RouterModule.forRoot(routes);
