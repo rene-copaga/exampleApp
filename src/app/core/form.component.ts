987f4f84bf2ea8dd38a0fb116cae5a4bcfaa5fef
Rene Copaga <rene.copaga@gmail.com>
2020-06-15 09:36:45 -0400

    Handling Route Changes in Components

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -5,6 +5,7 @@ import { Model } from "../model/repository.model";
 import { MODES, SharedState, SHARED_STATE } from "./sharedState.model";
 import { Observable } from "rxjs";
 import { filter, map, distinctUntilChanged, skipWhile } from "rxjs/operators";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: "paForm",
@@ -13,22 +14,9 @@ import { filter, map, distinctUntilChanged, skipWhile } from "rxjs/operators";
 })
 export class FormComponent {
     product: Product = new Product();
-    // lastId: number;
 
-    constructor(private model: Model,
-        @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
-        stateEvents
-        // .pipe(skipWhile(state => state.mode == MODES.EDIT))
-        // .pipe(distinctUntilChanged((firstState, secondState) =>
-        //     firstState.mode == secondState.mode
-        //         && firstState.id == secondState.id))
-        .subscribe(update => {
-            this.product = new Product();
-            if (update.id != undefined) {
-                Object.assign(this.product, this.model.getProduct(update.id));
-            }
-            this.editing = update.mode == MODES.EDIT;
-        });
+    constructor(private model: Model, activeRoute: ActivatedRoute) {
+        this.editing = activeRoute.snapshot.url[1].path == "edit";
     }
     
     editing: boolean = false;
@@ -44,14 +32,4 @@ export class FormComponent {
     resetForm() {
         this.product = new Product();
     }
-
-    // ngDoCheck() {
-    //     if (this.lastId != this.state.id) {
-    //         this.product = new Product();
-    //         if (this.state.mode == MODES.EDIT) {
-    //             Object.assign(this.product, this.model.getProduct(this.state.id));
-    //         }
-    //         this.lastId = this.state.id
-    //     }
-    // }
 }
