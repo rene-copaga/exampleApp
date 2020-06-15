8f86439283b79c2ce7c753af2b84d8c16bd3b7f9
Rene Copaga <rene.copaga@gmail.com>
2020-06-15 15:25:22 -0400

    Receiving Navigation Events

--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { Router, NavigationEnd, NavigationCancel } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
     selector: "paMessages",
@@ -9,7 +11,11 @@ import { Message } from "./message.model";
 export class MessageComponent {
     lastMessage: Message;
 
-    constructor(messageService: MessageService) {
+    constructor(messageService: MessageService, router: Router) {
         messageService.messages.subscribe(m => this.lastMessage = m);
+        router.events
+            .pipe(filter(e => e instanceof NavigationEnd
+                || e instanceof NavigationCancel))
+            .subscribe(e => { this.lastMessage = null })
     }
 }
