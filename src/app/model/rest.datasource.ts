206c043c90e83d60562eaa04d0b566ed4d02ee6b
Rene Copaga <rene.copaga@gmail.com>
2020-06-16 17:24:45 -0400

    Delaying Navigation with a Resolver

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -2,7 +2,7 @@ import { Inject, InjectionToken, Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { Product } from "./product.model";
-import { catchError } from "rxjs/operators";
+import { catchError, delay } from "rxjs/operators";
 
 export const REST_URL = new InjectionToken("rest_url");
 
@@ -36,7 +36,9 @@ export class RestDataSource {
         return this.http.request<T>(verb, url, {
             body: body,
             headers: myHeaders
-        }).pipe(catchError((error: Response) =>
+        })
+        .pipe(delay(5000))
+        .pipe(catchError((error: Response) =>
             throwError(`Network Error: ${error.statusText} (${error.status})`)));
     }
 }
