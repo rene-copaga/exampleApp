673faa1ba5dbdf8d1313f6f16aa85028e18ebaac
Rene Copaga <rene.copaga@gmail.com>
2020-06-20 16:05:23 -0400

    Creating the Animation

---
+++ src/app/core/table.animations.ts
@@ -0,0 +1,14 @@
+import { trigger, state, style, transition, animate } from "@angular/animations";
+
+export const HighlightTrigger = trigger("rowHighlight", [
+    state("selected", style({
+        backgroundColor: "lightgreen",
+        fontSize: "20px"
+    })),
+    state("notselected", style({
+        backgroundColor: "lightsalmon",
+        fontSize: "12px"
+    })),
+    transition("selected => notselected", animate("200ms")),
+    transition("notselected => selected", animate("400ms"))
+]);
