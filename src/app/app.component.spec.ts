74b410542bdba74ac6784ac46f4e7e18ff9842f4
Rene Copaga <rene.copaga@gmail.com>
2020-06-21 16:04:54 -0400

    Working with Jasmine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
 describe("Jasmine Test Environment", () => {
-  it("is working", () => expect(true).toBe(false));
+  it("test numeric value", () => expect(12).toBeGreaterThan(10));
+  it("test string value", () => expect("London").toMatch("^Lon"));
 });
