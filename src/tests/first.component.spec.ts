fbd3561fdab3596367ab2218ab593de1df9920ce
Rene Copaga <rene.copaga@gmail.com>
2020-06-21 18:22:11 -0400

    Working with the TestBed Class

---
+++ src/tests/first.component.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { FirstComponent } from "src/app/ondemand/first.component";
+
+describe("FirstComponent", () => {
+    let fixture: ComponentFixture<FirstComponent>;
+    let component: FirstComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [FirstComponent]
+        });
+        fixture = TestBed.createComponent(FirstComponent);
+        component = fixture.componentInstance;
+    });
+
+    it("is defined", () => {
+        expect(component).toBeDefined()
+    });
+});
