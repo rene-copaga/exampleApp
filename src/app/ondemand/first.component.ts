b784b92ae95c3e760278145053ae5348285bdeb5
Rene Copaga <rene.copaga@gmail.com>
2020-06-21 18:38:48 -0400

    Configuring the Test Bed for Dependencies

--- src/app/ondemand/first.component.ts
+++ src/app/ondemand/first.component.ts
@@ -1,7 +1,23 @@
 import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { Model } from "../model/repository.model";
 
 @Component({
     selector: "first",
-    template: `<div class="bg-primary text-white p-2">First Component</div>`
+    template: `<div class="bg-primary text-white p-2">
+                    There are
+                        <span class="strong"> {{getProducts().length}} </span>
+                    products
+                </div>`
 })
-export class FirstComponent { }
+export class FirstComponent {
+
+    constructor(private repository: Model) {}
+
+    category: string = "Soccer";
+
+    getProducts(): Product[] {
+        return this.repository.getProducts()
+            .filter(p => p.category == this.category);
+    }
+}
--- src/tests/first.component.spec.ts
+++ src/tests/first.component.spec.ts
@@ -1,13 +1,28 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { FirstComponent } from "src/app/ondemand/first.component";
+import { Product } from "src/app/model/product.model";
+import { Model } from "src/app/model/repository.model";
 
 describe("FirstComponent", () => {
     let fixture: ComponentFixture<FirstComponent>;
     let component: FirstComponent;
 
+    let mockRepository = {
+        getProducts: function () {
+            return [
+                new Product(1, "test1", "Soccer", 100),
+                new Product(2, "test2", "Chess", 100),
+                new Product(3, "test3", "Soccer", 100),
+            ]
+        }
+    }
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [FirstComponent]
+            declarations: [FirstComponent],
+            providers: [
+                { provide: Model, useValue: mockRepository }
+            ]
         });
         fixture = TestBed.createComponent(FirstComponent);
         component = fixture.componentInstance;
@@ -15,5 +30,14 @@ describe("FirstComponent", () => {
 
     it("is defined", () => {
         expect(component).toBeDefined()
+    });
+
+    it("filters categories", () => {
+        component.category = "Chess";
+        expect(component.getProducts().length).toBe(1);
+        component.category = "Soccer";
+        expect(component.getProducts().length).toBe(2);
+        component.category = "Running";
+        expect(component.getProducts().length).toBe(0);
     });
 });
